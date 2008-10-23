c99b95d2207c917af27f5c7e07e8cb40a7864b56
Jari Aalto <jari.aalto@cante.net>
2008-10-24 01:03:39 +0300

    etc/lib/cygbuild.pl: VERSION update

--- etc/lib/cygbuild.pl
+++ etc/lib/cygbuild.pl
@@ -88,7 +88,7 @@ use vars qw ( $VERSION );
 #   The following variable is updated by Emacs setup whenever
 #   this file is saved.
 
-$VERSION = '2008.0820.0902';
+$VERSION = '2008.1023.2202';
 
 # ..................................................................
 
