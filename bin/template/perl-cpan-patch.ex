752b56cb9358d99cb3c0d0ea03e0327ea0519203
Jari Aalto <jari.aalto@cante.net>
2009-12-13 01:40:19 +0200

    bin/template/perl-cpan-patch.ex: new

---
+++ bin/template/perl-cpan-patch.ex
@@ -0,0 +1,35 @@
+# Changes needed for Perl files that need external CPAN libraries
+#
+# 1. Remove "use" calls from original program
+# 2. Add BEGIN block like below
+
+-use Mail::Box::Manager;
+-use Mail::Message;
+-use Date::Format;
+-use Date::Parse;
+ use Getopt::Long qw(:config no_ignore_case bundling);
+
+BEGIN
+{
+    my @list = qw
+    (
+        Mail::Box::Manager
+ 	Mail::Message
+        Date::Format
+        Date::Parse
+    );
+
+    for ( @list )
+    {
+        eval "use $_;" ;
+        push @list, $_   if $@;  # Eval error
+    }
+
+    if ( @list )
+    {
+        die q([FATAL] Perl CPAN modules are needed. Please install them with:
+
+              perl -MCPAN -e 'install $_ for @ARGV' ) . qq(@list\n);
+    }
+}
+
