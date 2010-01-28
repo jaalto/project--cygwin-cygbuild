8ded88b559ec4b140c91c9cb508a685d4897faa5
Jari Aalto <jari.aalto@cante.net>
2010-01-28 14:17:35 +0200

    bin/cygbuild.sh: define global PATH

--- bin/cygbuild.sh
+++ bin/cygbuild.sh
@@ -97,6 +97,9 @@ set -o pipefail     # status comes from the failed pipe command
 
 LC_ALL=C            # So that sort etc. works as expected.
 LANG=C
+PATH="/usr/bin:/usr/lib:/usr/sbin:/bin:/sbin"
+
+unset -f awk egrep grep tar perl
 
 #######################################################################
 #
