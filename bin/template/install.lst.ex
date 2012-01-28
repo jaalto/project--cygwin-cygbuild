10fb48fe9437dc7c80ec299d8f8c0119ce54842a
Jari Aalto <jari.aalto@cante.net>
2012-01-28 23:48:02 +0200

    bin/template/install.lst.ex: improve comment

--- bin/template/install.lst.ex
+++ bin/template/install.lst.ex
@@ -1,4 +1,12 @@
-# List install(1) components: FROM TO [MODE, default=755]
-# Without trailing slash: rename file TO. $PKG is documentation dir. Examples:
-#    binary usr/bin/bin-new
-#    README $PKG/doc/ 644
+# Syntax: FROM [TO [MODE; */bin/* defaults to 755]]
+#
+# Without trailing slash: rename file TO. TO expansions can use
+# variables $PKG $VER $DOC. Scripts and manpages are automatically
+# installed. Script suffixes *.pl etc. are stripped on install.
+#
+#    program usr/bin/
+#    prg     usr/bin/newname
+#    README  $DOC/ 644
+#    man.1
+#    program.pl
+
