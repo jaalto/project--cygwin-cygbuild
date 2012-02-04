321332547da782d89cc816d9dd47378fd0b50916
Jari Aalto <jari.aalto@cante.net>
2012-02-04 10:35:37 +0200

    clean.sh.ex: New

---
+++ bin/template/clean.sh.ex
@@ -0,0 +1,10 @@
+#!/bin/sh
+#
+# clean.sh -- Custom clean script
+#
+#   Called as: CYGWIN-PATCHES/clean.sh <absolute-install-dir>
+
+PATH="/sbin:/usr/sbin/:/bin:/usr/bin"
+LC_ALL="C"
+
+# End of file
