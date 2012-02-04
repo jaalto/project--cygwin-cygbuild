3ef022067162cb73b643c68a0c27595e416f9f98
Jari Aalto <jari.aalto@cante.net>
2012-02-04 10:52:13 +0200

    distclean.sh.ex: New

---
+++ bin/template/distclean.sh.ex
@@ -0,0 +1,10 @@
+#!/bin/sh
+#
+# distclean.sh -- Custom distclean script
+#
+#   Called as: CYGWIN-PATCHES/distclean.sh <absolute-install-dir>
+
+PATH="/sbin:/usr/sbin/:/bin:/usr/bin"
+LC_ALL="C"
+
+# End of file
