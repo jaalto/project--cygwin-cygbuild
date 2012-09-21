2773fdf6efdbee2ea6b1cb997f476861a8056569
Jari Aalto <jari.aalto@cante.net>
2012-09-21 23:41:56 +0300

    bin/template/cygwin-announce.mail.ex: (License): Use (at) sign

--- bin/template/cygwin-announce.mail.ex
+++ bin/template/cygwin-announce.mail.ex
@@ -30,11 +30,11 @@ system. Then, run setup and answer all of the questions. You'll find
 the package listed in the "All" category. After installation, read the
 documentation at directories:
 
-    /usr/share/doc/<package-version>/*
+    /usr/share/doc/<package>/*
     /usr/share/doc/Cygwin/<package-version>.README
 
 If you have questions or comments, please send them to the Cygwin
-mailing list at <cygwin@cygwin.com>.
+mailing list at <cygwin(at)cygwin.com>.
 
 CYGWIN-ANNOUNCE UNSUBSCRIBE INFO
 ================================
@@ -45,7 +45,7 @@ If you want to unsubscribe from the mailing list, look at the
 "List-Unsubscribe: " tag in the email header of this message. Send
 email to the address specified there. It will be in the format:
 
-    cygwin-announce-unsubscribe-you=yourdomain.com@cygwin.com
+    cygwin-announce-unsubscribe-you=yourdomain.com(at)cygwin.com
 
 More information on unsubscribing can be found:
 
