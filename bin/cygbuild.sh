f305ff2a1141031c44886115e6ce382da922144b
Jari Aalto <jari.aalto@cante.net>
2012-09-23 13:04:42 +0300

    (CygbuildCmdTestMain): Adjust topic print line
    Signed-off-by: Jari Aalto <jari.aalto@cante.net>

--- bin/cygbuild.sh
+++ bin/cygbuild.sh
@@ -48,7 +48,7 @@ CYGBUILD_NAME="cygbuild"
 
 #  Automatically updated by the developer's editor on save
 
-CYGBUILD_VERSION="2012.0923.0726"
+CYGBUILD_VERSION="2012.0923.1004"
 
 #  Used by the 'cygsrc' command to download official Cygwin packages
 #  listed at http://cygwin.com/packages
@@ -9295,7 +9295,7 @@ function CygbuildCmdTestMain()
 
     # FIXME Do we have a need to return proper status code?
 
-    CygbuildEcho "== Test"
+    CygbuildEcho "== Test command"
 
     CygbuildPushd
 	cd "$builddir" || CygbuildDie "$id: [builddir] error"
