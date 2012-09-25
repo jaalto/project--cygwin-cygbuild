059c9d33b4a4fc19530f794e0ace1b246b22fffa
Jari Aalto <jari.aalto@cante.net>
2012-09-25 23:14:11 +0300

    bin/template/test.sh.ex: new
    Signed-off-by: Jari Aalto <jari.aalto@cante.net>

--- bin/cygbuild.sh
+++ bin/cygbuild.sh
@@ -48,7 +48,7 @@ CYGBUILD_NAME="cygbuild"
 
 #  Automatically updated by the developer's editor on save
 
-CYGBUILD_VERSION="2012.0925.1919"
+CYGBUILD_VERSION="2012.0925.1925"
 
 #  Used by the 'cygsrc' command to download official Cygwin packages
 #  listed at http://cygwin.com/packages
