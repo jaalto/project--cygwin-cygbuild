af30152f2ae0bf81936d47ac42f941fb7f8c56f7
Jari Aalto <jari.aalto@cante.net>
2012-10-03 10:58:00 +0300

    install.lst.ex: Add ln example

--- bin/template/install.lst.ex
+++ bin/template/install.lst.ex
@@ -1,11 +1,12 @@
 # Syntax: FROM [TO [MODE]]
 #
-# Without trailing slash: rename file TO; TO expansions $PKG $VER DOC
+# Without trailing slash: rename file TO. You can use: $PKG $VER $DOC
 # Scripts and manpages are automatically installed; no TO is needed:
 # Script suffixes *.pl etc. are stripped on install.
 #
 #    program usr/bin/
-#    prg     usr/bin/newname
+#    program usr/bin/newname
+#    ln usr/bin/program aliasname
 #    README  $DOC/ 644
 #    man.1
 #    program.pl
