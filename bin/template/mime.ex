5ac7e33785811be25f9541753b4768ea59898072
Jari Aalto <jari.aalto@cante.net>
2012-10-09 16:37:30 +0300

    mime.ex: New

---
+++ bin/template/mime.ex
@@ -0,0 +1,4 @@
+# This file is installed to /usr/lib/mime/packages/PACKAGE
+# See mailcap(1) manual page for format.
+# Remove all these comments to add entries like:
+# application/msword; catdoc %s; copiousoutput; description=MS Doc Text;
