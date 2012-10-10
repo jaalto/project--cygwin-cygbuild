92840b952f56d8b8600d36957fde0fdf58bc2f39
Jari Aalto <jari.aalto@cante.net>
2012-10-10 15:47:32 +0300

    install.lst.ex: Add mkdir

--- bin/template/install.lst.ex
+++ bin/template/install.lst.ex
@@ -1,12 +1,14 @@
-# Syntax: FROM [TO [MODE]]
+# Basic syntax: FROM [TO [MODE]]
 #
 # Without trailing slash: rename file TO. You can use: $PKG $VER $DOC
 # Scripts and manpages are automatically installed; no TO is needed:
-# Script suffixes *.pl etc. are stripped on install.
+# Script suffixes *.pl etc. are stripped on install. Creaete symbolic
+# links with "ln" command or directories with "mkdir". See below:
 #
 #    program usr/bin/
 #    program usr/bin/newname
 #    ln usr/bin/program aliasname
+#    mkdir var/log/program
 #    README  $DOC/ 644
 #    man.1
 #    program.pl
