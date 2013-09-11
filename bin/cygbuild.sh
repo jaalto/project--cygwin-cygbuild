52d5a43df2f4975222101d937096e2de189cd775
Jari Aalto <jari.aalto@cante.net>
2013-09-11 16:29:41 +0300

    (CygbuildCmdConfConfigFilesCopy): use Echo()
    Signed-off-by: Jari Aalto <jari.aalto@cante.net>

--- bin/cygbuild.sh
+++ bin/cygbuild.sh
@@ -48,7 +48,7 @@ CYGBUILD_NAME="cygbuild"
 
 #  Automatically updated by the developer's editor on save
 
-CYGBUILD_VERSION="2013.0911.1142"
+CYGBUILD_VERSION="2013.0911.1327"
 
 #  Used by the 'cygsrc' command to download official Cygwin packages
 #  listed at http://cygwin.com/packages
@@ -9120,7 +9120,7 @@ function CygbuildCmdConfConfigFilesCopy()
             done=done
             destdir=$(CygbuildFindConfigFileDir)
 
-            CygbuildVerb "-- Copying custom config.{sub,guess} files"
+            CygbuildEcho "-- Copying custom config.{sub,guess} files"
 
             if [ ! "$destdir" ]; then
                 CygbuildWarn "-- [ERROR] Can't locate configu.guess directory"
