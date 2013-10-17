cb2886bad963225f4a47e2a89f55b64d0e170a56
Jari Aalto <jari.aalto@cante.net>
2013-10-17 08:25:37 +0300

    bin/template/build.options.ex: add -Werror=implicit-function-declaration

--- bin/template/build.options.ex
+++ bin/template/build.options.ex
@@ -1,8 +1,8 @@
 # Just before make(1) is about to be run, this file is sourced
 # CYGBUILD_CC="gcc"
 # CYGBUILD_CXX="g++"
-# CYGBUILD_CFLAGS="-O2 -g"
-# CYGBUILD_CXXLAGS="-O2 -g"
+# CYGBUILD_CFLAGS="-O2 -ggdb -pipe -Werror=implicit-function-declaration"
+# CYGBUILD_CXXLAGS="-O2 -ggdb -pipe -Werror=implicit-function-declaration"
 #
 # set to -Wl,--no-undefined for libraries
 # CYGBUILD_LDFLAGS=""
