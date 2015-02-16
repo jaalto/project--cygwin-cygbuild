2b4dd55bd7ab738796e1d09550168cb1df8985bd
Jari Aalto <jari.aalto@cante.net>
2015-02-17 00:24:05 +0200

    configure-before.sh.ex: new

---
+++ bin/template/configure-before.sh.ex
@@ -0,0 +1,11 @@
+#!/bin/sh
+# Copyright (C) YYYY Firstname Lastname; Licensed under GPL v2 or later
+#
+# configure-before.sh -- Custom configure script
+#
+#   Called as: CYGWIN-PATCHES/configure-before.sh <absolute-install-dir>
+
+PATH="/sbin:/usr/sbin/:/bin:/usr/bin"
+LC_ALL="C"
+
+# End of file
