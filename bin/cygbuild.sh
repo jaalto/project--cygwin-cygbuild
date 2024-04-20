29691e19dbb5f8eacc6787556546e41943e9bd3c
Jari Aalto <jari.aalto@cante.net>
2024-04-20 16:35:52 +0300

    rm newline

--- bin/cygbuild.sh
+++ bin/cygbuild.sh
@@ -48,7 +48,7 @@ CYGBUILD_NAME="cygbuild"
 
 #  Automatically updated by the developer's editor on save
 
-CYGBUILD_VERSION="2024.0420.1332"
+CYGBUILD_VERSION="2024.0420.1335"
 
 #  Used by the 'cygsrc' command to download official Cygwin packages
 #  listed at http://cygwin.com/packages
@@ -13233,7 +13233,6 @@ function CygbuildCommandMain()
                     CygbuildWarn "-- [WARN] Libs found." \
                         "Did you mean [package-devel]?"
                 fi
-
 
                 if [ ! "$OPTION_SIGN" ]; then
                     CygbuildWarn "[ERROR] --sign option missing"
