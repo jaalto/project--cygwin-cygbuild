67c734720b43bc9330f50e79fc3ccbc52a2b2979
Jari Aalto <jari.aalto@cante.net>
2025-02-06 00:36:14 +0200

    Update copyright years
    Signed-off-by: Jari Aalto <jari.aalto@cante.net>

--- bin/cygbuild.sh
+++ bin/cygbuild.sh
@@ -2,7 +2,7 @@
 #
 #   cygbuild.sh -- A generic Cygwin Net Release package builder script
 #
-#       Copyright (C) 2003-2024 Jari Aalto
+#       Copyright (C) 2003-2025 Jari Aalto
 #
 #   License
 #
